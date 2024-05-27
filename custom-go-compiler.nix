00d24a981a715ec31d53ddf79d98ec0b2f928db8
guillem.cordoba <guillem.cordoba@gmail.com>
2024-05-27 16:21:28 +0200

    Deterministic git in custom-go

--- custom-go-compiler.nix
+++ custom-go-compiler.nix
@@ -61,16 +61,21 @@
       go = lib.overrideDerivation pkgs.go_1_21 (attrs: rec {
         name = "go-${version}-dev";
         version = "1.21";
-        src = pkgs.fetchgit {
-          url = "https://github.com/wlynxg/go";
-          rev = "bff8d409ebfb8d4c8488325f13cb212b07cf6bb4";
-          sha256 = if pkgs.stdenv.isLinux then
-            "NTVa7gGnPnTGfBWElRuiZs0f8FHqux0jCDRvceZiBSI="
-          else
-            "8xt45wLNUGo4zBduEh3lGDkxzpAnd47AEuluJNlRbgc=";
-          # sha256 = "NTVa7gGnPnTGfBWElRuiZs0f8FHqux0jCDRvceZiBSI=";
-          leaveDotGit = true;
-        };
+        src = let
+          gitSrc = pkgs.fetchgit {
+            url = "https://github.com/wlynxg/go";
+            rev = "bff8d409ebfb8d4c8488325f13cb212b07cf6bb4";
+            sha256 = "i5MnEkFSEhy+D4C+Syyc0Xkch248VD75ccvQlsMB/6U=";
+          };
+          finalGo = pkgs.runCommandNoCC "custom-go" { } ''
+            mkdir $out
+            cd ${gitSrc}
+            cp -R . $out
+            ls $out
+
+            echo "${version}" > $out/VERSION
+          '';
+        in finalGo;
         buildInputs = with pkgs; [ pcre git ];
         nativeBuildInputs = with pkgs; [ pcre git ];
       });
