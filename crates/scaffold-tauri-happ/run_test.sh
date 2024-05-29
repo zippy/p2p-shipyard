ef596af9fa87e9fffee1f04d16add19389e9bbbf
guillem.cordoba <guillem.cordoba@gmail.com>
2024-05-29 20:06:29 +0200

    Fixed android compilation

--- crates/scaffold-tauri-happ/run_test.sh
+++ crates/scaffold-tauri-happ/run_test.sh
@@ -31,5 +31,5 @@ set -e
 
 npm i
 npm run tauri android init
-cargo build --target aarch64-linux-android
+TAURI_ANDROID_PACKAGE_NAME_PREFIX=myorg TAURI_ANDROID_PACKAGE_NAME_APP_NAME=mypackage cargo build --target aarch64-linux-android
 "
